8fba5ff69849cc6d8541fce5f0d0abb73916d940
Divya Rani <101196300+divy08r@users.noreply.github.com>
2023-07-18 16:23:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, url_for, redirect
 import pickle
 import Health_predict as hp
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.preprocessing import OneHotEncoder
 
 encoded_cols = hp.encoded_cols
 encoder = hp.encoder
@@ -28,10 +32,6 @@ def predict():
         int_features=[x for x in request.form.values()]
         finalarray=[np.array(int_features)]
         def input(finalarray):
-            import pandas as pd
-            from sklearn.preprocessing import StandardScaler
-            from sklearn.preprocessing import MaxAbsScaler
-            from sklearn.preprocessing import OneHotEncoder
             final = {
                 "gender" : finalarray[0][0].lower(),
                 "age"  : (float)(finalarray[0][1]),
